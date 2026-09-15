4ae93687a9dabfee2a407f206fe1a6b466dc0522
andrespalbuj <andrespalbuj@hotmail.com>
2026-09-15 14:05:33 +0200

    Cat reformadao

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-// Tu código aquí
+//1. Buscamos la foto y los textos en el DOM:
+
+const foto = document.querySelector("#photo");
+const textoNombre = document.querySelector("#first");
+const textoApellido = document.querySelector("#last");
+const textoPais = document.querySelector("#country");
+const textoTelefono = document.querySelector("#phone");
+const textoEmail = document.querySelector("#email");
+
+//2. Buscamos los dos botones:
+
+const botonGenerar = document.querySelector("#btn");
+
+//3. Buscamos el cuerpo de la tabla:
+
+const cuerpoTabla = document.querySelector("#table tbody");
+
+//4. Buscamos a los NUEVOS AMIGOS:
+
+
+
